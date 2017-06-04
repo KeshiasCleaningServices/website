35e2f715e9ba9a9f9a16e1bf4416c1b2e2737fde
Keshia's Cleaning Services <keshiascleaningservices@gmail.com>
2017-06-04 10:07:41 -0400

    Add files via upload

---
+++ javascript/init.js
@@ -0,0 +1,12 @@
+(function ($) {
+    'use strict';
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.tooltipped').tooltip({delay: 50});
+    $('.modal-trigger').leanModal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: 0.5, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200 // Transition out duration  
+    });// end of document ready
+})(jQuery); // end of jQuery name space
